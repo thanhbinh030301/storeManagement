ecb097db8e8885160a71a38ef5a7d72ab36c95a4
binhthanh <thanhbinhnguyen097@gmail.com>
2022-06-02 11:34:36 +0700

    update

--- src/StoreManagement/DAO/SanPhamDAO.java
+++ src/StoreManagement/DAO/SanPhamDAO.java
@@ -15,16 +15,16 @@ import java.util.ArrayList;
  */
 public class SanPhamDAO {
         
-    SanPham sp1 = new SanPham(1, "banh", 20, "cai", 3000);
-    SanPham sp2 = new SanPham(2, "keo", 10, "cai", 3000);
-    SanPham sp3 = new SanPham(3, "nuoc ngot", 30, "chai", 3000);
-    SanPham sp4 = new SanPham(4, "muoi", 30, "kg", 4000);
-    SanPham sp5 = new SanPham(5, "duong", 40, "kg", 3000);
-    SanPham sp6 = new SanPham(6, "rau", 50, "kg", 1000);
-    SanPham sp7 = new SanPham(7, "oi", 30, "kg", 3000);
-    SanPham sp8 = new SanPham(8, "man", 70, "kg", 2000);
+    SanPham sp1 = new SanPham("1", "banh", 20, "cai", 3000);
+    SanPham sp2 = new SanPham("2", "keo", 10, "cai", 3000);
+    SanPham sp3 = new SanPham("3", "nuoc ngot", 30, "chai", 3000);
+    SanPham sp4 = new SanPham("4", "muoi", 30, "kg", 4000);
+    SanPham sp5 = new SanPham("5", "duong", 40, "kg", 3000);
+    SanPham sp6 = new SanPham("6", "rau", 50, "kg", 1000);
+    SanPham sp7 = new SanPham("7", "oi", 30, "kg", 3000);
+    SanPham sp8 = new SanPham("8", "man", 70, "kg", 2000);
 
-     public ArrayList<SanPham> getListSanPham() {
+    public ArrayList<SanPham> getListSanPham() {
         ArrayList<SanPham> dssp = new ArrayList<>();
         dssp.add(sp1);
         dssp.add(sp2);
