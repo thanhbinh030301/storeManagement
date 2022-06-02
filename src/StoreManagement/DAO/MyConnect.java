1d6c0455a7462936d0ec58f0cb6f265254749156
HoangHung <conbodu68@gmail.com>
2022-06-02 16:36:04 +0700

    add connect

--- src/StoreManagement/DAO/MyConnect.java
+++ src/StoreManagement/DAO/MyConnect.java
@@ -8,16 +8,20 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class MyConnect {
-    
+    Connection connect = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
     
     public static Connection getJDBCConection() {
         final String url = "jdbc:mysql://localhost:3306/storemanagementdb";
         final String user = "root";
-        final String password = "332001";
+        final String password = "123456";
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -28,15 +32,69 @@ public class MyConnect {
             Logger.getLogger(MyConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
+    } protected Statement getStatement() throws Exception {
+//        kiểm tra stament đã đóng chưa
+        if (this.statement == null) {
+//            khởi tạo statement mới
+            this.statement = getJDBCConection().createStatement();
+        }
+        else {
+            this.statement.isClosed();
+        }
+        return this.statement;
     }
-    public static void main(String[] args) {
-        Connection conn = getJDBCConection();
-        if(conn!=null){
-            System.out.println("THanh cong");
-        }else {
-            System.out.println("That bai");
+    
+    //hàm thực thi các câu lệnh SQl 
+    public ResultSet excuteQuery(String Query) throws Exception{
+        try {
+            // thực thi câu lệnh
+            this.resultSet = getStatement().executeQuery(Query);
+            
+        } catch (Exception e) {
+            throw new Exception("Error excuteQuery " + e.getMessage());
+        }
+        
+        return this.resultSet;
+    }
+    
+//    thực thi các Insert, Update, Delete
+    public int executeUpdate(String Query) throws Exception {
+        //khai báo biến int để lưu trữ kết quả torng quá trình thực thi
+        int res = Integer.MIN_VALUE;
+        
+        try {
+            //thực thi câu lệnh
+            res = getStatement().executeUpdate(Query);
+        } catch (Exception e) {
+            throw new Exception("Error " + e.getMessage());
+        }
+        
+        return res;
+    }
+    
+    //hàm đóng kết nối
+    public void Close() throws Exception {
+        if (this.resultSet != null && this.resultSet.isClosed()) {
+            this.resultSet.close();
+            this.resultSet = null;
+        }
+        if (this.statement != null && this.statement.isClosed()) {
+            this.statement.close();
+            this.statement = null;
+        }
+        if (this.connect != null && this.connect.isClosed()) {
+            this.connect.close();
+            this.connect = null;
         }
     }
+//    public static void main(String[] args) {
+//        Connection conn = getJDBCConection();
+//        if(conn!=null){
+//            System.out.println("Ket noi database thanh cong");
+//        }else {
+//            System.out.println("Ket noi database that bai");
+//        }
+//    }
     
 
 //    public static Connection conn = null;
