649c2f61ace824209116c59bddcf84c6c967b485
binhthanh <thanhbinhnguyen097@gmail.com>
2022-06-14 15:13:35 +0700

    update

--- src/StoreManagement/GUI/DlgCapTK.java
+++ src/StoreManagement/GUI/DlgCapTK.java
@@ -14,7 +14,7 @@ public class DlgCapTK extends javax.swing.JDialog {
         txtName.setText(PnNhanVien.nvSelected.getTen());
     }
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")  
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
